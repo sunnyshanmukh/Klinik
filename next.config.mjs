9b8beb8db83da465e24d3fe1cddd00d766bdb44e
sunnyshanmukh <sunnyshanmukh8978@gmail.com>
2025-12-18 22:06:26 +0530

    undo changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack(config, { isServer }) {
-    if (!isServer) {
-      config.optimization.splitChunks.cacheGroups = {
-        ...config.optimization.splitChunks.cacheGroups,
-        styles: {
-          name: 'styles',
-          type: 'css/mini-extract',
-          chunks: 'all',
-          enforce: true,
-        },
-      };
-    }
-    return config;
-  },
   /* config options here */
 };
 
